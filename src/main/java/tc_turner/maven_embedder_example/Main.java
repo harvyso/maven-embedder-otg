ab575349742d7209d97fe219331e196bee6c6413
Mitchell Turner <mturner@threatconnect.com>
2017-04-06 13:54:55 -0400

    Added every all from dependency:tree

--- src/main/java/tc_turner/maven_embedder_example/Main.java
+++ src/main/java/tc_turner/maven_embedder_example/Main.java
@@ -4,9 +4,10 @@ import org.apache.maven.cli.MavenCli;
 
 public class Main {
 
-	public static void main(String[] args) {
-		MavenCli cli = new MavenCli();
-		cli.doMain( args, ".", System.out, System.err );
-	}
+    public static void main(String[] args) {
+        System.setProperty("maven.multiModuleProjectDirectory", ".");
+        MavenCli cli = new MavenCli();
+        cli.doMain(args, ".", System.out, System.err);
+    }
 
 }
